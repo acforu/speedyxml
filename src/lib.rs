235435b0473a5b743385d3985481387aff29bf36
acforu <newltf@163.com>
2017-12-17 22:17:47 +0800

    modify

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::str::FromStr;
 
-
-
 // struct document
 // {
 
@@ -26,11 +24,19 @@ struct XmlAttr {
 enum XmlParseError {
 	element_name_error,
 	expect_attr_name,
+	expect_attr_equal,
+	expect_attr_value,
+	unknow_error,
+	unimplemented,
 }
 
 enum XmlElement {
-	XmlNode { name: XmlStr, attrs: Vec<XmlAttr> },
-	XmlAttrs {attrs: Vec<XmlAttr>},
+	XmlNode { name: XmlStr, attrs: Vec<XmlAttr> , children:Vec<XmlNode>},
+	XmlAttrs { attrs: Vec<XmlAttr> },
+}
+
+struct XmlDocument{
+	node: Vec<XmlNode>
 }
 
 type ParseResult = Result<XmlElement, XmlParseError>;
@@ -38,7 +44,6 @@ type ParseResult = Result<XmlElement, XmlParseError>;
 //     println!("{}", unsafe { std::intrinsics::type_name::<T>() });
 // }
 static name_end_chars: &'static [char] = &[' ', '\n', '\r', '\t', '/', '>', '?', '\0'];
-
 
 // Attribute name (anything but space \n \r \t / < > = ? ! \0)
 static invalid_attr_name_chars: &'static [char] =
@@ -76,7 +81,6 @@ pub fn parse(filename: &str) {
 		if xml[pos] == '<' as u8 {
 			parse_node(&mut pos, xml);
 		} else {
-			//#todo throw error
 			break;
 		}
 	}
@@ -98,7 +102,6 @@ fn parse_element(pos: &mut usize, xml: &[u8]) -> ParseResult {
 		return Err(XmlParseError::element_name_error);
 	}
 
-
 	let node_name: XmlStr = XmlStr {
 		beg: name_beg,
 		end: *pos,
@@ -107,15 +110,33 @@ fn parse_element(pos: &mut usize, xml: &[u8]) -> ParseResult {
 	skip_whitespace(pos, xml);
 
 	let attrResult = parse_node_attr(pos, xml);
-	if attrResult.is_err()
-	{
-		return attrResult.unwrap_err();
+	if attrResult.is_err() {
+		return Err(attrResult.err().unwrap());
 	}
 
-	return Ok(XmlElement::XmlNode(node_name,attrResult.unwrap());
+	if xml[*pos] == '>' as u8 {
+		//parse content
+		advance(pos);
+		return Err(XmlParseError::unimplemented);
+	} else if xml[*pos] == '/' as u8 {
+		advance(pos);
+		if xml[*pos] != '>' as u8 {
+			return Err(XmlParseError::unknow_error); //expected >
+		}
+		advance(pos);
+	} else {
+		return Err(XmlParseError::unknow_error); //expected >
+	}
+
+	return Ok(XmlElement::XmlNode {
+		name: node_name,
+		attrs: attrResult.ok().unwrap(),
+	});
 }
 
-fn parse_node_attr(pos: &mut usize, xml: &[u8]) -> ParseResult {
+fn parse_node_attr(pos: &mut usize, xml: &[u8]) -> Result<Vec<XmlAttr>, XmlParseError> {
+	let mut ret = Vec::new();
+
 	loop {
 		let c = xml[*pos] as char;
 		if in_chars_set(c, invalid_attr_name_chars) {
@@ -128,18 +149,59 @@ fn parse_node_attr(pos: &mut usize, xml: &[u8]) -> ParseResult {
 		if *pos == attr_name_beg {
 			return Err(XmlParseError::expect_attr_name);
 		}
+
+		let attr_name = XmlStr {
+			beg: attr_name_beg,
+			end: *pos,
+		};
+
+		skip_whitespace(pos, xml);
+
+		if xml[*pos] != '=' as u8 {
+			return Err(XmlParseError::expect_attr_equal);
+		}
+
+		advance(pos); //skip =
+		skip_whitespace(pos, xml);
+		let attr_value_beg = *pos;
+		let tag = xml[*pos];
+		if tag != '"' as u8 || tag != '\'' as u8 {
+			return Err(XmlParseError::expect_attr_value);
+		}
+
+		skip_until(tag as char, pos, xml);
+		let attr_value = XmlStr {
+			beg: attr_value_beg,
+			end: (*pos) - 1,
+		};
+		ret.push(XmlAttr {
+			name: attr_name,
+			value: attr_value,
+		});
+
+		skip_whitespace(pos, xml);
 	}
-	Ok(())
+
+	return Ok(ret);
+}
+
+fn advance(pos: &mut usize) {
+	*pos = *pos + 1;
 }
 
 fn in_chars_set(c: char, set: &[char]) -> bool {
 	return set.iter().any(|&x| x == c);
 }
 
-
 fn skip_name(pos: &mut usize, xml: &[u8]) {
 	let c = xml[*pos] as char;
 	while !name_end_chars.iter().any(|&x| x == c) {
+		*pos = *pos + 1;
+	}
+}
+
+fn skip_until(target: char, pos: &mut usize, xml: &[u8]) {
+	while xml[*pos] != target as u8 {
 		*pos = *pos + 1;
 	}
 }
@@ -159,8 +221,6 @@ fn skip_chars(skip_chars_set: &[char], pos: &mut usize, xml: &[u8]) {
 fn skip_whitespace(pos: &mut usize, xml: &[u8]) {
 	skip_char(' ', pos, xml);
 }
-
-
 
 pub fn test() {
 	// println!("hello world" );
